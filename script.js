bc94e73c5348ef3b1dd5b987f50592668fd05e2d
Rachel ✨ <rachelajayi08@gmail.com>
2022-10-26 04:41:33 +0100

    another commit

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
 //grab access to body element
 const body = document.querySelector("body");
+// const fetch = require("isomorphic-fetch")
 
 async function fetchPokemon() {
   //saving the api response in a constant
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
   const data = await response.json();
-  const p = document.createElement("p");
-  p.innerHTML = data.name;
-  body.appendChild(p);
-}
+  let variable = data["results"]
+ 
+  // card.classList.add("card")
+
+  for (let i = 0; i < variable.length; i++) {
+    let cards = document.querySelector(".cards")
+    let card = document.querySelector(".card")
+    let p = document.createElement("p");
+    p.innerText = variable[i]["name"] 
+    let img = document.createElement("img")
+    img.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`)
+    card.append(p)
+    card.append(img)
+   cards.append(card)
+    // body.append(p)
+    // body.append(cards)
+    
+  }
+ 
+ }
 
 fetchPokemon();
-
-//Callback example for success/error handling
-// function funcName(successCallback, errorCallback) {
-//   let num = 1 + 2;
-//   if (num === 2) {
-//     successCallback("Success");
-//   } else {
-//     errorCallback("Failed");
-//   }
-// }
-
-//funcName takes 2 args, success callback and error callback, both will console.log the str
-
-//Promise version of funcName
-// function funcName() {
-//   let num = 1 + 1;
-//   //return a debt that we will fulfill
-//   return new Promise((resolve, reject) => {
-//     if (num === 2) {
-//       resolve("Success");
-//     } else {
-//       reject("Failed");
-//     }
-//   });
-// }
-
-// funcName()
-//   .then((response) => {
-//     console.log(response);
-//     console.log("More Stuff");
-//   })
-//   .catch((error) => console.log(error));
